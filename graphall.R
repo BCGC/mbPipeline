454cb9628e77ec4d367280ae8de6afed3876be79
nikhilbg <nikhilbg96@gmail.com>
2014-07-11 11:17:11 -0400

    fixed issue with graphics data reading
    sep = ‘\t’ was missing

--- graphall.R
+++ graphall.R
@@ -5,20 +5,20 @@ args <- commandArgs(TRUE)
 
 library("mbGraphics")
 
-meta.frame <- read.table("mb_graphics_data.txt", skip=1, header=TRUE, stringsAsFactors=FALSE)
+meta.frame <- read.table("mb_graphics_data.txt", skip=1, header=TRUE, stringsAsFactors=FALSE, sep='\t')
 data.types <- strsplit(readLines("mb_graphics_data.txt")[1], '\t')[[1]]
 adiv(meta.frame, data.types)
 
-meta.frame <- read.table("mb_graphics_data.txt", skip=1, header=TRUE, stringsAsFactors=FALSE)
+meta.frame <- read.table("mb_graphics_data.txt", skip=1, header=TRUE, stringsAsFactors=FALSE, sep='\t')
 data.types <- strsplit(readLines("mb_graphics_data.txt")[1], '\t')[[1]]
 beta.frame <- read.table("beta_data.out", header=TRUE, stringsAsFactors=FALSE)
 bdiv(meta.frame, data.types, beta.frame)
 
-meta.frame <- read.table("mb_graphics_data.txt", skip=1, header=TRUE, stringsAsFactors=FALSE)
+meta.frame <- read.table("mb_graphics_data.txt", skip=1, header=TRUE, stringsAsFactors=FALSE, sep='\t')
 data.types <- strsplit(readLines("mb_graphics_data.txt")[1], '\t')[[1]]
 numseq(meta.frame, data.types)
 
-meta.frame <- read.table("mb_graphics_data.txt", skip=1, header=TRUE, stringsAsFactors=FALSE)
+meta.frame <- read.table("mb_graphics_data.txt", skip=1, header=TRUE, stringsAsFactors=FALSE, sep='\t')
 data.types <- strsplit(readLines("mb_graphics_data.txt")[1], '\t')[[1]]
 tax.key <- read.table(args[1], header = TRUE, stringsAsFactors = FALSE)
 tax.data <- read.table(args[2], header = TRUE, stringsAsFactors = FALSE)
