a317ecf67838b277393eeb29d7a899dd58c52470
Nikhil Gowda <gowdanb@mail.nih.gov>
2015-07-13 10:36:56 -0400

    completed defaults setup (for 454)

--- launcher.py
+++ launcher.py
@@ -3,7 +3,7 @@ import os
 
 args = dict()
 for a in sys.argv[1:len(sys.argv)]:
-      args[a[0:a.find('=')]] = a[a.find('=')+1:len(a)]
+    args[a[0:a.find('=')]] = a[a.find('=')+1:len(a)]
 
 print('\nArgs:')
 print(args)
@@ -13,104 +13,122 @@ installed = os.path.dirname(os.path.realpath(sys.argv[0]))
 
 ######### SETUP DEFAULT VALUES #############
 try:
-      pipeline = args['pipeline']
+    PATH = args['workdir']
+    if len(PATH) > 0:
+        os.chdir(PATH)
 except KeyError:
-      raise Exception("Pipeline mode not propvided!")
-if pipeline!='454' & pipeline!='miseq':
-    raise Exception("Proper pipeline name not specified!")
-
-try:
-      PATH = args['workdir']
-      if len(PATH) > 0:
-            os.chdir(PATH)
-except KeyError:
-      print("Using default working directory.")
+    print("Using default working directory.")
 except OSError:
-      print("Invalid value for workdir, using default directory.")
+    print("Invalid value for workdir, using default directory.")
+    
+with open('run.json', 'r+') as f:
+            run = json.load(f)
+            
+            try:
+                pipeline = args['pipeline']
+                run["setup"]["pipeline"] = pipeline
+            except KeyError:
+                raise Exception("Pipeline mode not propvided!")
+            if pipeline != '454' & pipeline != 'miseq':
+                raise Exception("Proper pipeline name not specified!")
 
-      
-try:
-      metadata = args['metadata']
-except KeyError:
-      raise Exception("Metadata file not propvided!")
+            try:
+                metadata = args['metadata']
+                run["setup"]["metadata"] = metadata
+            except KeyError:
+                raise Exception("Metadata file not propvided!")
 
-try:
-      controlsfile = args['controls']
-      arecontrols = True
-except KeyError:
-      arecontrols = False
-      print("Warning: No controls file was provided, assuming that there are none.")
+            try:
+                controlsfile = args['controls']
+                run["setup"]["controlsfile"] = controlsfile
+                run["setup"]["arecontrols"] = "1"
+            except KeyError:
+                run["setup"]["arecontrols"] = "0"
+                print("Warning: No controls file was provided, assuming that there are none.")
 
-# link reference files to the current working directory
-try:
-      REFPATH = args['refpath']
-except KeyError:
-      REFPATH = os.system("echo $MBREF")
-      if REFPATH == 0 :
-            REFPATH = "."
-            print("Warning: Reference data path not specified! Will try and use default directory.")
+            # link reference files to the current working directory
+            try:
+                REFPATH = args['refpath']
+            except KeyError:
+                REFPATH = os.system("echo $MBREF")
+                if REFPATH == 0 :
+                    REFPATH = "."
+                    print("Warning: Reference data path not specified! Will try and use default directory.")
 
-if os.path.isdir(REFPATH):
-      os.system("ln -fs " + REFPATH + "/oligos.txt .")
-      os.system("ln -fs " + REFPATH + "/trainset* .")
-      os.system("ln -fs " + REFPATH + "/LookUp_Titanium.pat .")
-      os.system("ln -fs " + REFPATH + "/silva.* .")
-      os.system("ln -fs " + REFPATH + "/" + metadata + "")
-      os.system("ln -fs " + REFPATH + "/" +  controlsfile + "")
-else:
-      raise Exception("Bad value for refpath.")
+            if os.path.isdir(REFPATH):
+                os.system("ln -fs " + REFPATH + "/oligos.txt .")
+                os.system("ln -fs " + REFPATH + "/trainset* .")
+                os.system("ln -fs " + REFPATH + "/LookUp_Titanium.pat .")
+                os.system("ln -fs " + REFPATH + "/silva.* .")
+                os.system("ln -fs " + REFPATH + "/" + metadata + "")
+                os.system("ln -fs " + REFPATH + "/" +  controlsfile + "")
+            else:
+                raise Exception("Bad value for refpath.")
 
-# link data files to the current working directory
-try:
-      DATAPATH = args['datapath']
-except KeyError:
-      DATAPATH = "."
-      print("Warning: Data path for sff files not specified! Will try and use deafault directory.")
+            # link data files to the current working directory
+            try:
+                DATAPATH = args['datapath']
+                run["setup"]["datapath"] = DATAPATH
+            except KeyError:
+                DATAPATH = "."
+                print("Warning: Data path for sff files not specified! Will try and use deafault directory.")
 
-if not os.path.isdir(DATAPATH):
-      raise Exception("Bad value for datapath.")
+            if not os.path.isdir(DATAPATH):
+                raise Exception("Bad value for datapath.")
+            
+            try:
+                trainset = args['trainset']
+                run["setup"][pipeline]["trainset"] = trainset
+            except KeyError:
+                print("Warning: Using default trainset!")
 
-try:
-    trainset = args['trainset']
-except KeyError:
-    #
-    print("Warning: Using default trainset!")
+
+            # make sure we have the project name ###
+            try:
+                proj = args['project']
+                run["setup"]["proj"] = proj
+            except KeyError:
+                raise Exception("Missing project name!")
+
+            if pipeline == "454":
+                # pdiffs
+                try:
+                    pdiffs = args['pdiffs']
+                    run["setup"][pipeline]["pdiffs"] = pdiffs
+                except KeyError:
+                    print("Using default pdiffs.")
+
+                # bdiffs
+                try:
+                    bdiffs = args['bdiffs']
+                    run["setup"][pipeline]["bdiffs"] = bdiffs
+                except KeyError:
+                    print("Using default bdiffs")
+
+            try:
+                indvars = args['indvars']
+                if not type(indvars) is list:
+                    print("Warning: indvars is not a list, will use all independent variables in metadata (cat. and cont.) for graphing!")
+                else:
+                    run["setup"]["indvars"] = indvars
+            except KeyError:
+                print("Will use all independent variables in metadata (cat. and cont.) for graphing!")
+
+            try:
+                min_stack_proportion = args['min_stack_proportion']
+                run["setup"]["min_stack_proportion"] = min_stack_proportion
+            except KeyError:
+                print("Minimum stack proportion not provided, will use default!")
+
+            try:
+                nprocessors = args['processors']
+                run["setup"]["nprocessors"] = nprocessors
+            except KeyError:
+                print("Number of processors not provided, will use default")
 
 
-# make sure we have the project name ###
-try:
-      proj = args['project']
-except KeyError:
-      raise Exception("Missing project name!")
+            f.seek(0)
+            f.write(json.dumps(run))
+            f.truncate()
 
-# pdiffs
-try:
-      pdiffs = args['pdiffs']
-except KeyError:
-      pdiffs = '2'
 
-# bdiffs
-try:
-      bdiffs = args['bdiffs']
-except KeyError:
-      bdiffs = '1'
-
-try:
-      indvars = args['indvars']
-      if not type(indvars) is list:
-            print("Warning: indvars is not a list, will use all independent variables in metadata (cat. and cont.) for graphing!")
-except KeyError:
-      indvars = []
-      print("Will use all independent variables in metadata (cat. and cont.) for graphing!")
-
-try:
-      min_stack_proportion = args['min_stack_proportion']
-except KeyError:
-      min_stack_proportion = "0.14"
-      print("Minimum stack proportion not provided, will default to 0.14!")
-
-try:
-      nprocessors = args['processors']
-except KeyError:
-      nprocessors = 1
-      print("Number of processors not provided, will default to 1!")
